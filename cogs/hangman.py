a287cffa18903194f8e4078b9090a9645e49f430
NateM135 <melwaninate@gmail.com>
2020-11-29 21:22:31 -0800

    Fixed several hangman bugs, all messages are 10x cleaner, game is fully functional

--- cogs/hangman.py
+++ cogs/hangman.py
@@ -25,8 +25,15 @@ class Hangman(commands.Cog):
     @commands.Cog.listener()
     async def on_message(self, message):
         if str(message.channel.id) in self.games:
-            if message.content.lower() == self.games[str(message.channel.id)][0].lower():
-                await message.channel.send("Correct! The word was " + self.games[str(message.channel.id)][0])
+            if message.content.lower() == self.games[str(message.channel.id)][4].lower():
+
+                #await message.channel.send("Correct! The word was " + self.games[str(message.channel.id)][4].title())
+
+                embed=discord.Embed(title="You Guessed It!", description="Good Job " + message.author.mention + "!", color=0xdd293e)
+                embed.add_field(name="The correct word is:", value=self.games[str(message.channel.id)][4].title(), inline=False)
+                embed.set_footer(text="Hangman - Category: " + self.games[str(message.channel.id)][1].title())
+                await message.channel.send(embed=embed)
+
                 del self.games[str(message.channel.id)]
             else:
                 if len(message.content) == 1:
@@ -44,17 +51,50 @@ class Hangman(commands.Cog):
                             self.games[str(message.channel.id)][2] = self.games[str(message.channel.id)][2]
 
                         self.games[str(message.channel.id)][2]=temp
-                        await message.channel.send("Correct!\n" + self.games[str(message.channel.id)][2])
-                        #fixme
+                        #await message.channel.send("Correct!\n" + self.games[str(message.channel.id)][2])
 
-                        if "_" not in self.games[str(message.channel.id)][2]:
-                            await message.channel.send("**You did it!** The correct word is: " + self.games[str(message.channel.id)][2].title())
+                        embed=discord.Embed(title="Correct!", description=message.author.mention + " has guessed a letter correctly.", color=0xdd293e)
+                        embed.add_field(name="Current Progress:", value=self.games[str(message.channel.id)][2], inline=False)
+                        embed.set_footer(text="Hangman - Category: " + self.games[str(message.channel.id)][1].title())
+                        await message.channel.send(embed=embed)
+
+
+
+
+                        if "◯" not in self.games[str(message.channel.id)][2]:
+
+                            #await message.channel.send("**You did it!** The correct word is: " + self.games[str(message.channel.id)][4].title())
+
+                            embed=discord.Embed(title="You Guessed It!", description="Good Job " + message.author.mention + "!", color=0xdd293e)
+                            embed.add_field(name="The correct word is:", value=self.games[str(message.channel.id)][4].title(), inline=False)
+                            embed.set_footer(text="Hangman - Category: " + self.games[str(message.channel.id)][1].title())
+                            await message.channel.send(embed=embed)
+
                             del self.games[str(message.channel.id)]
                     else:
+                        #here
+                        if message.content in self.games[str(message.channel.id)][2]:
+                            await message.channel.send("debug: this lettern has already been guessed correctly")
+                            return
+
+
                         self.games[str(message.channel.id)][3] = self.games[str(message.channel.id)][3] + 1
-                        await message.channel.send("Incorrect! " + str(message.content) + " is not in the word! Guesses Left: " + str(6-self.games[str(message.channel.id)][3]))
+                        #await message.channel.send("Incorrect! " + str(message.content) + " is not in the word! Guesses Left: " + str(6-self.games[str(message.channel.id)][3]))
+
+                        embed=discord.Embed(title="Incorrect!", description="Nice Try " + message.author.mention + "!", color=0xdd293e)
+                        embed.add_field(name="\'" +str(message.content) + "\'" + " is not in the word!", value="Guesses Left: " + str(6-self.games[str(message.channel.id)][3]), inline=False)
+                        embed.set_footer(text="Hangman - Category: " + self.games[str(message.channel.id)][1].title())
+                        await message.channel.send(embed=embed)
+
+
                         if self.games[str(message.channel.id)][3] == 6:
-                            await message.channel.send("You lost :(\n The correct word is: " + self.games[str(message.channel.id)][0].title())
+                            #await message.channel.send("You lost :(\n The correct word is: " + self.games[str(message.channel.id)][4].title())
+
+                            embed=discord.Embed(title="You Lost :(", description="You have run out of guesses.", color=0xdd293e)
+                            embed.add_field(name="The correct word is:", value=self.games[str(message.channel.id)][4].title(), inline=False)
+                            embed.set_footer(text="Hangman - Category: " + self.games[str(message.channel.id)][1].title())
+                            await message.channel.send(embed=embed)
+
                             del self.games[str(message.channel.id)]
                             
 
@@ -62,8 +102,15 @@ class Hangman(commands.Cog):
     
     @commands.command()
     async def hangman(self, ctx, *, arg=None):
+        if arg!=None and arg.lower() == "categories":
+            categs = ""
+            for x in self.words:
+                categs = categs + x.title() + "\n"
 
-
+            embed=discord.Embed(title="Categories", description=categs.strip(), color=0xdd293e)
+            embed.set_footer(text="Hangman - Zeta Bot")
+            await ctx.send(embed=embed)
+            return
 
         #Create Game Instance
         if str(ctx.channel.id) not in self.games:
@@ -73,27 +120,40 @@ class Hangman(commands.Cog):
                 category = random.choice(list(self.words))
                 random_category = True
             else:
-                if category.lower() in self.words:
-                    category = args
+                if arg.lower() in self.words:
+                    category = arg.lower()
                 else:
-                    await ctx.send(arg + " is not a valid category! To see all categories, type this command that is not implemented yet!")
+                    embed=discord.Embed(title="Invalid Category", description= arg + " is not a valid category! To see all categories, type ``<prefix>hangman categories``!", color=0xdd293e)
+                    embed.set_footer(text="Hangman - Zeta Bot")
+                    await ctx.send(embed=embed)
                     return
             word = random.choice(self.words[category])
+            unchanged_word=word
 
             if random_category:
-                await ctx.send("Starting a game of hangman. Random Category: " + category.title())
+                embed=discord.Embed(title="Starting a Game of Hangman", description="Random Category: " + category.title(), color=0xdd293e)
+                embed.set_footer(text="Hangman - Category: " + category.title())
+                await ctx.send(embed=embed)
             else:
-                await ctx.send("Starting a game of hangman. Selected Category: " + category.title())
+                embed=discord.Embed(title="Starting a Game of Hangman", description="Selected Category: " + category.title(), color=0xdd293e)
+                embed.set_footer(text="Hangman - Category: " + category.title())
+                await ctx.send(embed=embed)
 
             guess_str = "```"
             for x in word:
                 if x == " ":
-                    guess_str+=" "
+                    guess_str+="  "
                 else:
-                    guess_str+="_"
+                    guess_str+="◯ "
             guess_str = guess_str + "```"
+
+            temp_str = ""
+            for letter in word:
+                temp_str = temp_str + letter + " "
             guess_str=guess_str.strip()
-            self.games[str(ctx.channel.id)] = ["```" + word.lower() + "```",category.title(), guess_str, 0]
+            word = temp_str
+            word = word.strip()
+            self.games[str(ctx.channel.id)] = ["```" + word.lower() + "```",category.title(), guess_str, 0, unchanged_word]
             print(self.games)
         
         #Send Game Progress
