59d2d376953e7a1b88024a86c73d8829e5781a7a
NateM135 <melwaninate@gmail.com>
2020-12-11 22:33:00 -0800

    Embedded Invite, UserInfo works for other users besides yourself

--- cogs/general.py
+++ cogs/general.py
@@ -38,27 +38,53 @@ class General(commands.Cog):
 
     @commands.command()
     async def invite(self, ctx):
-        await ctx.send("Want to invite the bot to your server? Use this link: https://discord.com/oauth2/authorize?client_id=705283045573394493&scope=bot&permissions=270400")
+        embed=discord.Embed(title="Click here to invite the bot to your server!", url="https://discord.com/oauth2/authorize?client_id=705283045573394493&scope=bot&permissions=270400")
+        await ctx.send(embed=embed)
 
     @commands.command()
     async def tutorial(self, ctx):
-        await ctx.send("Want to learn how to use the bot? Watch this video: https://www.youtube.com/watch?v=dHQdAGczV5A")
+        await ctx.send("Want to learn how to use the autoprune feature of the bot? Watch this video: https://www.youtube.com/watch?v=dHQdAGczV5A")
 
-    @commands.command()
-    async def userinfo(self, ctx):
-        role_str = ""
-        for r in ctx.author.roles:
-            role_str += r.name + ", "
-        role_str = role_str[:-2]
-        embed=discord.Embed(title=f"Information For: {ctx.author.display_name}")
-        embed.set_thumbnail(url=ctx.author.avatar_url)
-        embed.add_field(name="ID", value=str(ctx.author.id), inline=True)
-        embed.add_field(name="Username", value=ctx.author.name, inline=True)
-        embed.add_field(name="Bot", value="False", inline=True)
-        embed.add_field(name="Account Creation", value=ctx.author.created_at.strftime("%m/%d/%Y, %H:%M:%S"), inline=True)
-        embed.add_field(name="Mention", value=ctx.author.mention, inline=True)
-        embed.add_field(name="Roles", value=role_str, inline=True)
-        await ctx.send(embed=embed)
+
+    @commands.command(
+        name="userinfo",
+        description="Posts information about a user.",
+        aliases=['uinfo']
+    )
+    async def userinfo(self, ctx, user: discord.Member=None):
+        if user == None:
+            role_str = ""
+            
+            for r in ctx.author.roles:
+                if r.name == '@everyone':
+                    continue
+                role_str += r.name + ", "
+            role_str = role_str[:-2]
+            embed=discord.Embed(title=f"Information For: {ctx.author.display_name}")
+            embed.set_thumbnail(url=ctx.author.avatar_url)
+            embed.add_field(name="ID", value=str(ctx.author.id), inline=True)
+            embed.add_field(name="Username", value=ctx.author.name, inline=True)
+            embed.add_field(name="Bot", value="False", inline=True)
+            embed.add_field(name="Account Creation", value=ctx.author.created_at.strftime("%m/%d/%Y, %H:%M:%S"), inline=True)
+            embed.add_field(name="Mention", value=ctx.author.mention, inline=True)
+            embed.add_field(name="Roles", value=role_str, inline=True)
+            await ctx.send(embed=embed)
+        else:
+            role_str = ""
+            for r in user.roles:
+                if r.name == '@everyone':
+                    continue
+                role_str += r.name + ", "
+            role_str = role_str[:-2]
+            embed=discord.Embed(title=f"Information For: {user.display_name}")
+            embed.set_thumbnail(url=user.avatar_url)
+            embed.add_field(name="ID", value=str(user.id), inline=True)
+            embed.add_field(name="Username", value=user.name, inline=True)
+            embed.add_field(name="Bot", value="False", inline=True)
+            embed.add_field(name="Account Creation", value=user.created_at.strftime("%m/%d/%Y, %H:%M:%S"), inline=True)
+            embed.add_field(name="Mention", value=user.mention, inline=True)
+            embed.add_field(name="Roles", value=role_str, inline=True)
+            await ctx.send(embed=embed)
 
     
 def setup(bot):
