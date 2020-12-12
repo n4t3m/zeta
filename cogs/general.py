64d692e1e98582933b23e796b25de3f22b28f3f0
NateM135 <melwaninate@gmail.com>
2020-12-11 19:29:09 -0800

    updated general commands, added userinfo, embedded stats and ping

--- cogs/general.py
+++ cogs/general.py
@@ -16,8 +16,9 @@ class General(commands.Cog):
     
     @commands.command()
     async def ping(self, ctx):
-        await ctx.send('Pong! {0}'.format(round(self.bot.latency, 1)))
-        await ctx.send("Unfortunately all data has been lost (11/1). Please reconfigure your servers to keep using the bot. When you set a delay, that delay only applies to one specific channel, so you must set the delay for each channel you want to use the bot with.")
+        embed=discord.Embed()
+        embed.add_field(name="Ping", value='Pong! {0}'.format(round(self.bot.latency, 1)))
+        await ctx.send(embed=embed)
 
     @commands.command()
     async def stats(self, ctx):
@@ -25,11 +26,15 @@ class General(commands.Cog):
         memcount = 0
         for g in self.bot.guilds:
             try:
-                print(g.name + " " + str(g.member_count))
-                memcount = memcount + g.member_count
+                if member_count > 1000:
+                    print(g.name + " " + str(g.member_count))
+                    memcount = memcount + g.member_count
             except:
                 pass
-        await ctx.send("Guilds: " + str(gcount) + "\nMembers: " + str(memcount))
+        embed=discord.Embed(title="Bot Statistics")
+        embed.add_field(name="Guilds", value=str(gcount), inline=False)
+        embed.add_field(name="Users", value=str(memcount), inline=False)
+        await ctx.send(embed=embed)
 
     @commands.command()
     async def invite(self, ctx):
@@ -38,6 +43,22 @@ class General(commands.Cog):
     @commands.command()
     async def tutorial(self, ctx):
         await ctx.send("Want to learn how to use the bot? Watch this video: https://www.youtube.com/watch?v=dHQdAGczV5A")
+
+    @commands.command()
+    async def userinfo(self, ctx):
+        role_str = ""
+        for r in ctx.author.roles:
+            role_str += r.name + ", "
+        role_str = role_str[:-2]
+        embed=discord.Embed(title=f"Information For: {ctx.author.display_name}")
+        embed.set_thumbnail(url=ctx.author.avatar_url)
+        embed.add_field(name="ID", value=str(ctx.author.id), inline=True)
+        embed.add_field(name="Username", value=ctx.author.name, inline=True)
+        embed.add_field(name="Bot", value="False", inline=True)
+        embed.add_field(name="Account Creation", value=ctx.author.created_at.strftime("%m/%d/%Y, %H:%M:%S"), inline=True)
+        embed.add_field(name="Mention", value=ctx.author.mention, inline=True)
+        embed.add_field(name="Roles", value=role_str, inline=True)
+        await ctx.send(embed=embed)
 
     
 def setup(bot):
