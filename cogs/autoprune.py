eea79173d1fdb6d39f44c0af84fcdf99d045686c
NateM135 <melwaninate@gmail.com>
2022-06-14 12:12:35 -0700

    renamed .new -> .py

--- cogs/autoprune.py
+++ cogs/autoprune.py
@@ -1,197 +1,95 @@
+
 import discord
 from discord.ext import commands
-import random
+from discord.commands import slash_command, Option
 import json
-import asyncio
-import config
-import os
 
-class AutoPrune(commands.Cog):
+class Cog(commands.Cog):
+    def __init__(self, client):
+        self.client = client
 
-    def __init__(self, bot):
-        self.bot = bot
-        self.prefix = config.PREFIX
+    @slash_command(description="Get the ID of a user")
+    async def id(self, ctx, user: Option(discord.User, "User", required=False)):
+        user = user or ctx.author
+        await ctx.respond(f"The ID of {user} is {user.id}")
 
-    #Events
-
-    @commands.Cog.listener()
-    async def on_ready(self):
-        print(os.path.basename(__file__)[:-3].upper() + " loaded succesfully!")
-
-    @commands.Cog.listener()
-    async def on_message(self, message):
-        
-        with open('./ap_data/guilds.json') as json_file:
-            existingData = json.load(json_file)
-        data = existingData
-
-        if str(message.guild.id) not in data:
+    @slash_command(
+        name="addchannel",
+        description="Let this channel be pruned"
+        )
+    async def addchannel(self, ctx, channel: Option(discord.TextChannel, "Channel", required=True)):
+        if not ctx.author.guild_permissions.administrator:
+            await ctx.respond("You must be administrator to use this command", ephemeral=True)
             return
 
-        with open('./ap_data/delays.json') as json_file:
-            existingData = json.load(json_file)
-        t_data = existingData
-        
-        if message.content.lower().startswith("az!ignore") and message.author.guild_permissions.administrator:
-            return
-
-        if str(message.channel.id) in t_data:
-            d = t_data[str(message.channel.id)]
-            await self.remove_msg(message, d)
-            return
-
-    #Commands
-    
-    @commands.command()
-    async def addChannel(self, ctx):
-        if not ctx.message.author.guild_permissions.administrator:
-            await ctx.send("You must be administrator to use this command")
-            return
+        #channel = channel or ctx.channel
+        cid = channel.id
+        gid = channel.guild.id
 
         with open('./ap_data/guilds.json') as json_file:
-            existingData = json.load(json_file)
-        data = existingData
+            data = json.load(json_file)
 
-        if str(ctx.guild.id) not in data:
-            data[str(ctx.guild.id)] = []
+        if str(gid) not in data:
+            data[str(gid)] = []
 
-        if ctx.message.channel.id not in data[str(ctx.guild.id)]:
-            data[str(ctx.guild.id)].append(ctx.message.channel.id)
+        if cid not in data[str(gid)]:
+            data[str(gid)].append(cid)
         else:
-            print("This channel already has pruning enabled. Use the remove command in this channel if you would like to remove it.")
-        
+            await ctx.respond("This channel already has pruning enabled. Use the remove command in this channel if you would like to remove it.")
+            return
 
-        await ctx.send("Added " + ctx.message.channel.name + " to pruned channels. Use the channels command to see which channels are being pruned.")
+        await ctx.respond(f"Added {channel.name} to pruned channels. Use the channels command to see which channels are being pruned.")
 
         with open("./ap_data/guilds.json", "w") as write_file:
             json.dump(data, write_file)
         
         with open('./ap_data/delays.json') as json_file:
-            existingData = json.load(json_file)
-        data = existingData
+            data = json.load(json_file)
 
-        if str(ctx.channel.id) not in data:
-            data[str(ctx.channel.id)] = 300
+        if str(cid) not in data:
+            data[str(cid)] = 300
 
         with open("./ap_data/delays.json", "w") as write_file:
             json.dump(data, write_file)
 
-    @commands.command()
+    @slash_command(
+        name="channels",
+        description="List all channels being pruned in this server."
+        )
     async def channels(self, ctx):
 
         with open('./ap_data/guilds.json') as json_file:
-            existingData = json.load(json_file)
-        data = existingData
+            data = json.load(json_file)
 
-        if str(ctx.guild.id) not in data:
-            await ctx.send("This server has not been configured! Use the command ``addChannel`` to start pruning new messages!")
+        gid = ctx.guild.ix
+
+        if str(gid) not in data:
+            await ctx.respond("This server has not been configured! Use the command ``/addChannel`` to start pruning new messages!", ephemeral=True)
             return
-        
 
-        if len(data[str(ctx.guild.id)])==0:
-            await ctx.send("This server has no channels configured!")
+        if len(data[str(gid)])==0:
+            await ctx.respond("This server has no channels configured!", ephemeral=True)
             return
 
         message = "Channels Being Pruned: "
 
-        for x in data[str(ctx.guild.id)]:
+        for x in data[str(gid)]:
             try:
-                channel = self.bot.get_channel(x)
+                channel = self.client.get_channel(int(x))
                 message = message + channel.name + ", "
             except:
+                print('something happened here"')
                 pass
 
         message = message[:-2]
 
-        await ctx.send(message)
+        if len(data[str(gid)]) == 0:
+            message = message + "\nNone"
+
+        await ctx.respond(message, ephemeral=True)
 
         with open("./ap_data/guilds.json", "w") as write_file:
             json.dump(data, write_file)
 
-    @commands.command()
-    async def remove(self, ctx):
-
-        if not ctx.message.author.guild_permissions.administrator:
-            await ctx.send("You must be administrator to use this command")
-            return
-
-        with open('./ap_data/guilds.json') as json_file:
-            existingData = json.load(json_file)
-        data = existingData
-
-        if str(ctx.guild.id) not in data:
-            await ctx.send("This server has no channels configured!")
-            return
-
-        if ctx.message.channel.id not in data[str(ctx.guild.id)]:
-            await ctx.send("Channel cannot be removed as messages are not being removed here!")
-            return
-
-        data[str(ctx.guild.id)].remove(ctx.message.channel.id)
-
-        with open("./ap_data/guilds.json", "w") as write_file:
-            json.dump(data, write_file)
-
-        with open('./ap_data/delays.json') as json_file:
-            existingData = json.load(json_file)
-        data = existingData
-
-        if str(ctx.channel.id) not in data:
-            await ctx.send("This channel has not been configured!")
-            return
-
-        del data[str(ctx.channel.id)]
-        
-        await ctx.send("Removed channel: " + ctx.channel.name)
-
-        with open("./ap_data/delays.json", "w") as write_file:
-            json.dump(data, write_file)
-
-
-    @commands.command()
-    async def delay(self, ctx, d: int):
-        if not ctx.message.author.guild_permissions.administrator:
-            await ctx.send("You must be administrator to use this command")
-            return
-
-        with open('./ap_data/delays.json') as json_file:
-            existingData = json.load(json_file)
-        data = existingData
-
-        data[str(ctx.channel.id)] = d
-
-        await ctx.send("New Channel Delay Set: " + str(data[str(ctx.channel.id)]) + " seconds. Delays are now specific to channels. In each channel you want to prune in, you must set the delay using the delay command.")
-
-        with open("./ap_data/delays.json", "w") as write_file:
-            json.dump(data, write_file)
-
-    @commands.command()
-    async def checkdelay(self, ctx):
-        with open('./ap_data/delays.json') as json_file:
-            existingData = json.load(json_file)
-        data = existingData
-
-        if str(ctx.channel.id) not in data:
-            data[str(ctx.channel.id)] = 300
-        
-        await ctx.send("Current Channel's Delay: " + str(data[str(ctx.channel.id)]) + " seconds." )
-        
-        with open("./ap_data/delays.json", "w") as write_file:
-            json.dump(data, write_file)
-
-    async def remove_msg(self, message, delay):
-        c_name = message.channel.name
-        g_name = message.guild.name
-        await asyncio.sleep(delay) 
-        await message.delete()
-        print("A message has been automatically pruned in " + c_name + " in the server " + g_name)
-        return
-
-    @commands.command()
-    async def ignore(self, ctx):
-        return
-
-    
-    
-def setup(bot):
-    bot.add_cog(AutoPrune(bot))
+def setup(client):
+    client.add_cog(Cog(client))
